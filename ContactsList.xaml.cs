c279183042cf37484aa12f5a04abadf187dba0d0
devgopher <igevdo@mail.ru>
2016-11-15 23:18:28 +0300

    - ContactsList: already selected contacts are now on top of the list

--- ContactsList.xaml.cs
+++ ContactsList.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Xamarin.Forms;
+using System.Linq;
 
 namespace a15
 {
@@ -34,6 +35,8 @@ namespace a15
 						contOperator.AddPhone (cont.phone);
 				}
 			}
+
+			//this.
 		}
 
 		public ContactsList ()
@@ -48,6 +51,11 @@ namespace a15
 			contOperator = op;
 			contacts = contOperator.GetFromMobile ();
 
+			List<Contact> markedContacts = new List<Contact> ();
+
+			var usedPhones = contOperator.GetPhones ();
+			var usedSkypes = contOperator.GetSkypes ();
+
 			tableView = new TableView {
 				Intent = TableIntent.Form,
 				Root = new TableRoot
@@ -57,10 +65,45 @@ namespace a15
 
 			contCells.Clear ();
 
-			for (int i = 0; i < contacts.Count; ++i ) {
+			// в первую очередь высвечиваем ранее выбранные контакты
+			for (int i = 0; i < contacts.Count(); ++i) {
 				var cont = contacts [i];
 
 				if (cont.phone == "" || cont.phone == null)
+					continue;
+
+
+				if (usedPhones.Any ((x) => { return x == cont.phone; }) ||
+				    usedSkypes.Any ((x) => { return x == cont.skype; }) ) {
+					SwitchCell cntSwt = new SwitchCell ();
+					TableSection newTS = new TableSection { cntSwt };
+
+					tableView.Root.Add (newTS);
+					cntSwt.Text = cont.name;
+					cntSwt.On = true;
+
+					cont.isSelected = true;
+
+					cntSwt.OnChanged += (sender, e) => {
+						cont.isSelected = cntSwt.On;
+					};
+
+					contCells [newTS] = cntSwt;
+					markedContacts.Add ( contacts[i] );
+				}
+			}
+
+			// Далее выводим все прочие контакты
+			for (int i = 0; i < contacts.Count(); ++i ) {
+				if (contacts [i] == null)
+					continue;
+				var cont = contacts [i];
+
+				if (cont.phone == "" || cont.phone == null)
+					continue;
+
+				// Если контакт уже был выбран ранее - не показываем его
+				if (markedContacts.Any ((x) => (x == cont)))
 					continue;
 				
 				SwitchCell cntSwt = new SwitchCell ();
@@ -68,7 +111,7 @@ namespace a15
 
 				tableView.Root.Add (newTS);
 				cntSwt.Text = cont.name;
-				//cntSwt.On = cont.isSelected;
+				cntSwt.On = false;
 
 				cntSwt.OnChanged += (sender, e) => { 
 					cont.isSelected = cntSwt.On; 
@@ -78,7 +121,6 @@ namespace a15
 			}
 
 			ContactsLayout.Children.Add (tableView);
-
 		}
 
 		private Dictionary<TableSection, SwitchCell> contCells = new Dictionary<TableSection, SwitchCell> ();
